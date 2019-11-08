9961620a05a5e7084cf5e3238aaad2354887ec1d
Nhu Duc <ducnguyennhu410@gmail.com>
2019-11-08 21:58:59 +0700

    add signUp file

---
+++ SignUp/Script.js
@@ -0,0 +1,16 @@
+let user={
+    "firstName":"",
+    "lastName":"",
+    "dateOfBirth":"",
+    "gender":"",
+    "Email":"",
+    "contact-number":"",
+    "user-name":"",
+    "password":"",
+    "confirm-password":"",
+    "Job":""
+}
+
+
+
+
